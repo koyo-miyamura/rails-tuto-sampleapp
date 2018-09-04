ed4eebeefaaafbcd63fff7bbb62e53c66c0e2960
koyo <nexus9071@gmail.com>
2018-09-05 04:46:52 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@
     if @user && @user.authenticate(params[:session][:password]) # ユーザが存在&&パスワードが正しい
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user # user_url(user) と同じ
+      # redirect_to @user # user_url(user) と同じ
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'

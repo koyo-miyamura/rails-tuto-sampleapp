0de2f51bc60348818be16e8da752afa1071f6c76
koyo <nexus9071@gmail.com>
2018-09-06 06:42:18 +0900

    Add user microposts(skip 13.4.4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token
   before_save { email.downcase! } # selfは省略可なのでself.emailと同じ意味
   validates :name,  presence: true, length: { maximum: 50 }
@@ -35,5 +36,11 @@ class User < ApplicationRecord
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
+  end
+  
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 end
